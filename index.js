02799f2fe100d33a55e2b1037bc00e788e37b50f
moshiur rahman <moshiurcse888@gmail.com>
2021-09-26 11:53:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// const data = require('./friends_data.json');
+// console.log(data);
+
+// data.friends[0].name = "Anis";
+
+
+// console.log(data.friends[0].name);
+
+// delete data.friends[0].name;
+
+
+// console.log(data.friends[0].name);
+
+
+const data1 = require('./students_data.json');
+
+
+console.log(data1.students[1].language[2]);
